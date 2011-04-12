8802f1a0dcee884afcf5b606a870629a3687d55e
Randy Wilson <rwilson@ncsasports.org>
2011-04-12 12:38:14 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedCrayon::Application.config.secret_token = '3b221ce7512e1df4c9999370e001ec41181596ceedc4062b02c4d094444124d00e745641c67c0a769165a04ec0efa7a18fd4e37a563f1022f850e00c11a054dd'
